8774bd260fbfd9e9a66b3ae978ce66d473be0bea
Kensy Josabeth Ayala <kensyayaladev@outlook.com>
2025-04-08 11:10:38 -0600

    Changes by kensy
    Changes by kensy

--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/edwin/Desktop/mern-test/frontend/src/app/page.tsx
+// File: C:\Users\Kensy J. Ayala\source\repos\suscriptionTest\frontend\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
 
   const fetchTasks = () => {
-    fetch("http://localhost:3001/tasks", {
-      method: "POST",
-    })
+    fetch("http://localhost:3001/tasks")
       .then((res) => res.json())
-      .then((data) => setTasks(data))
+      .then((data) => {
+        debugger;
+        setTasks(data);
+        console.log(data);
+      })
       .catch((error) => console.error("Error fetching tasks:", error));
   };
 
@@ -15,10 +20,33 @@ export default function Home() {
     fetchTasks();
   }, []);
 
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
+
   const addTask = () => {
     fetch("http://localhost:3001/task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, completed: false }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setTitle("");
+        fetchTasks();
+      })
+      .catch((error) => console.error("Error adding task:", error));
+  };
+
+  const editTask = (id: string) => {
+    fetch(`http://localhost:3001/task/${id}`, {
       method: "PUT",
-      body: JSON.stringify({ title, date: Date.now() }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, completed: true, completedOn: new Date() }),
     })
       .then((res) => res.json())
       .then(() => {
@@ -36,8 +64,11 @@ export default function Home() {
       <ul>
         {tasks.map((task: any) => (
           <li key={task._id}>
-            {task.title} - {task.completed ? "Done" : "Pending"}
-            <div>Completed on:{task.completedOn}</div>
+            <span onClick={() => editTask(task._id)}>
+              <span>{task._id}</span> | 
+              {task.title} - {task.completed ? "Done" : "Pending"}
+              <div>Completed on:{task.completedOn}</div>
+            </span>
           </li>
         ))}
       </ul>
